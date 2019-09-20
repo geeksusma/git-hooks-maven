1f998695db521520cd9f1dee80b758d9d091fae7
j.villar@wata.es <j.villar@wata.es>
2019-09-20 09:47:29 +0200

    added a pre-push which run some mvn goals

---
+++ src/test/java/StupidTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class StupidTest {
+
+    @Test
+    public void should_alwaysGreen_when_executeTest() {
+        //then
+        assertTrue("Always green! Always green!", true);
+    }
+}
